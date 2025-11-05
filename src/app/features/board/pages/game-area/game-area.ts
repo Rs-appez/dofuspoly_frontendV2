4dbcc45fbeb2bcb4114ea1c02b00e6a89176f957
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-05 11:55:31 +0100

    use dice component/service

--- src/app/features/board/pages/game-area/game-area.ts
+++ src/app/features/board/pages/game-area/game-area.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Dice } from '@features/board/components/dice/dice';
+import { DiceService } from '@features/board/data/services/dice/dice.service';
 
 @Component({
   selector: 'app-game-area',
-  imports: [],
+  imports: [Dice],
   templateUrl: './game-area.html',
   styleUrl: './game-area.css',
 })
-export class GameArea { }
+export class GameArea {
+  private _diceService: DiceService = inject(DiceService);
+
+  rollDice(): void {
+    this._diceService.rollDice();
+  }
+}
