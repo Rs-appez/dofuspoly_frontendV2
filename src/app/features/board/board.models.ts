38286b5ff45d9bae077ad65fb7debfa728f13001
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-05 11:55:33 +0100

    add models

---
+++ src/app/features/board/board.models.ts
@@ -0,0 +1,27 @@
+export interface Game {
+  id: number;
+  board: { cases: Case[] };
+  players: Player[];
+  turn: number;
+  finished: boolean;
+  current_player: Player;
+}
+
+export interface Case {
+  name: string;
+  type: string;
+  color: string;
+  price: number;
+}
+
+export interface Player {
+  user: string;
+  position: number;
+  money: number;
+  in_jail: boolean;
+  jail_turns: number;
+  cards: Card[];
+  image: string;
+}
+
+export interface Card { }
