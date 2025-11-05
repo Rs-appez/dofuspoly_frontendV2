44ad1758dfccb5ef84aec25861e0305c0ee61c3c
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-05 16:57:48 +0100

    setup base login

--- src/app/features/login/pages/login/login.ts
+++ src/app/features/login/pages/login/login.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { Auth } from '@core/services/auth';
+import { StorageService } from '@core/services/storage.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
+export class Login implements OnInit {
+  private _formBuilder = inject(FormBuilder);
+  private _authService = inject(Auth);
+  private _storageService = inject(StorageService);
 
+  loginForm!: FormGroup;
+  ngOnInit(): void {
+    this.loginForm = this._formBuilder.group({
+      username: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  onSubmit(): void {
+    const username = this.loginForm.get('username')?.value;
+    const password = this.loginForm.get('password')?.value;
+    console.log('Login submitted with', { username, password });
+    // Here you would typically call an authentication service
+    this._authService.login(username, password).subscribe({
+      next: (response) => {
+        console.log('Login successful:', response);
+      },
+      error: (error) => {
+        console.error('Login failed:', error);
+      },
+    });
+  }
 }
