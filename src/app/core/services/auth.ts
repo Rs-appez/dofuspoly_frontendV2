74c9d25d44bddf12359a35eb07988341c79834b9
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-05 16:57:30 +0100

    update url

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -1,12 +1,17 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '@env/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
+  BASE_URL: string = `${environment.backUrl}`;
   private _http: HttpClient = inject(HttpClient);
   login(username: string, password: string) {
-    return this._http.post('/api/token/', { username, password });
+    return this._http.post(`${this.BASE_URL}/api/token/`, {
+      username,
+      password,
+    });
   }
 }
