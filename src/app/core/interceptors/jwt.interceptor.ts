03182dd10691511bbb64d363cd9e220f172eb44f
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-06 10:34:52 +0100

    handle refresh token

--- src/app/core/interceptors/jwt.interceptor.ts
+++ src/app/core/interceptors/jwt.interceptor.ts
@@ -1,13 +1,21 @@
 import { inject } from '@angular/core';
-import { HttpRequest, HttpEvent, HttpHandlerFn } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {
+  HttpRequest,
+  HttpEvent,
+  HttpHandlerFn,
+  HttpResponse,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { catchError, Observable, switchMap, throwError } from 'rxjs';
 import { StorageService } from '@core/services/storage.service';
+import { Auth } from '@core/services/auth';
 
 export function jwtInterceptor(
   request: HttpRequest<any>,
   next: HttpHandlerFn,
 ): Observable<HttpEvent<any>> {
   const storageService = inject(StorageService);
+  const auth = inject(Auth);
   const token = storageService.getJwt();
   if (token) {
     request = request.clone({
@@ -16,5 +24,31 @@ export function jwtInterceptor(
       },
     });
   }
-  return next(request);
+  return next(request).pipe(
+    catchError((err: HttpErrorResponse) => {
+      if (err.status === 401 && !request.url.endsWith('/refresh')) {
+        const refreshToken = storageService.getRefresh();
+        if (refreshToken) {
+          return auth.refreshToken(refreshToken).pipe(
+            switchMap((response) => {
+              storageService.setAccessToken(response.access);
+              const clonedRequest = request.clone({
+                setHeaders: {
+                  Authorization: `Bearer ${response.access}`,
+                },
+              });
+              return next(clonedRequest);
+            }),
+            catchError((refreshErr) => {
+              auth.logout();
+              return throwError(() => refreshErr);
+            }),
+          );
+        } else {
+          auth.logout();
+        }
+      }
+      return throwError(() => err);
+    }),
+  );
 }
