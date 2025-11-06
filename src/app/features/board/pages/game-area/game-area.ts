21529f35cfe37be80134c2b6fa547547747182c4
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-06 15:22:02 +0100

    add end btn in game area

--- src/app/features/board/pages/game-area/game-area.ts
+++ src/app/features/board/pages/game-area/game-area.ts
@@ -3,10 +3,11 @@ import { Player } from '@features/board/board.models';
 import { Dice } from '@features/board/components/dice/dice';
 import { GameService } from '@features/board/data/services/game/game.service';
 import { PlayerInfo } from '@features/board/components/player-info/player-info';
+import { EndTurnBtn } from '@features/board/components/end-turn-btn/end-turn-btn';
 
 @Component({
   selector: 'app-game-area',
-  imports: [Dice, PlayerInfo],
+  imports: [Dice, PlayerInfo, EndTurnBtn],
   templateUrl: './game-area.html',
   styleUrl: './game-area.css',
 })
