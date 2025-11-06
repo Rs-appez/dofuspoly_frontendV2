f97a9b883f308abc165a579f6a34c5980e3a3ad0
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-06 10:34:45 +0100

    add logout mehtod

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@env/environment';
+import { StorageService } from './storage.service';
 
 export interface LoginResponse {
   access: string;
@@ -12,11 +13,26 @@ export interface LoginResponse {
 })
 export class Auth {
   BASE_URL: string = `${environment.backUrl}`;
+
+  private _storageService = inject(StorageService);
   private _http: HttpClient = inject(HttpClient);
   login(username: string, password: string) {
     return this._http.post<LoginResponse>(`${this.BASE_URL}/api/token/`, {
       username,
       password,
     });
+  }
+
+  logout() {
+    this._storageService.delJwt();
+  }
+
+  refreshToken(refresh: string) {
+    return this._http.post<LoginResponse>(
+      `${this.BASE_URL}/api/token/refresh/`,
+      {
+        refresh,
+      },
+    );
   }
 }
