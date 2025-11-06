e23ae154aa831d2f5f566e8e867322ec4fee44f8
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-06 15:21:43 +0100

    display current player

--- src/app/features/board/components/player-info/player-info.ts
+++ src/app/features/board/components/player-info/player-info.ts
@@ -1,9 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, effect, inject } from '@angular/core';
+import { Player } from '@features/board/board.models';
 import { GameService } from '@features/board/data/services/game/game.service';
 
 @Component({
   selector: 'app-player-info',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './player-info.html',
   styleUrl: './player-info.css',
 })
@@ -11,4 +13,15 @@ export class PlayerInfo {
   private _gameService = inject(GameService);
 
   game$ = this._gameService.game$;
+  currentPlayer: Player | null = null;
+
+  constructor() {
+    effect(() => {
+      const game = this.game$();
+      if (game) {
+        this.currentPlayer = game.current_player;
+        console.log('Game update:', game);
+      }
+    });
+  }
 }
