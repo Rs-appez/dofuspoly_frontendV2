73861409ae722ad52dfb4d5c8815aee7ec35d7f5
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-07 17:44:00 +0100

    remove unused import

--- src/app/features/board/components/space/space.ts
+++ src/app/features/board/components/space/space.ts
@@ -1,4 +1,4 @@
-import { Component, computed, effect, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { Player, Space } from '@features/board/board.models';
 
 @Component({
