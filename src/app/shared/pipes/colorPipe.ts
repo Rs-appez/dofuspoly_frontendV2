4008993a80dbe69e2e9c6484c86b73febca0328f
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-12 12:23:35 +0100

    feat(pipe): add colorPipe for string normalization
    Introduced ColorPipe in shared pipes to normalize color strings by converting
    them to lowercase and replacing spaces with underscores. This will help
    standardize color values across the application.

---
+++ src/app/shared/pipes/colorPipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'colorPipe',
+})
+export class ColorPipe implements PipeTransform {
+  transform(value: string, ...args: unknown[]): string {
+    if (!value) {
+      return '';
+    }
+    value = value.toLowerCase().replaceAll(' ', '_');
+    return value;
+  }
+}
