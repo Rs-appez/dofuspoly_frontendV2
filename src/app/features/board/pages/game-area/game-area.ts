ccee6028db87e215061caa6cdac8f7686697aeb9
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-12 12:24:02 +0100

    feat(board): replace PlayerInfo with PlayerDetails component
    Updated the game area to use the new PlayerDetails component instead of
    PlayerInfo. Also imported PlayerDetails in the game-area page to ensure
    proper rendering and functionality.

--- src/app/features/board/pages/game-area/game-area.ts
+++ src/app/features/board/pages/game-area/game-area.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { GameService } from '@features/board/data/services/game/game.service';
 import { PlayerInfo } from '@features/board/components/player-info/player-info';
 import { Board } from '@features/board/components/board/board';
+import { PlayerDetails } from '@features/board/components/player-details/player-details';
 
 @Component({
   selector: 'app-game-area',
-  imports: [PlayerInfo, Board],
+  imports: [PlayerInfo, Board, PlayerDetails],
   templateUrl: './game-area.html',
   styleUrl: './game-area.css',
 })
