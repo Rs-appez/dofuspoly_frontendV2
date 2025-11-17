67cf81b68ffe1f077eef045ca4e8411da5b9f935
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-17 15:14:40 +0100

    faster roll

--- public/dices/generate.sh
+++ public/dices/generate.sh
@@ -1,1 +1,1 @@
-magick $(for i in $(seq 1 50); do echo dice_$(shuf -i 1-6 -n 1).png; done) -delay 5 -loop 0 -quality 100 dice_roll.webp
+magick $(for i in $(seq 1 50); do echo dice_$(shuf -i 1-6 -n 1).png; done) -delay 0.1 -loop 0 -quality 100 dice_roll.webp
