94004109cf5a5af128de9084665bfe7ca9e7dc3a
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-30 21:03:37 +0100

    add show card component

---
+++ src/app/features/board/components/show-card/show-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-card',
+  imports: [],
+  templateUrl: './show-card.html',
+  styleUrl: './show-card.css',
+})
+export class ShowCard {
+
+}
